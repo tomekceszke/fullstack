53e8471f286cd79355e8f27aeead10fb461ba6fe
tomekceszke <tomek@ceszke.com>
2019-09-03 22:51:25 +0200

    Initial commit

---
+++ helloworld-service/src/main/java/com/ceszke/fullstack/helloworldservice/HelloworldServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ceszke.fullstack.helloworldservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldServiceApplication.class, args);
+	}
+
+}
